2c42fa73951a0f8791b2afe5f3a5d6d616b3bbdc
Konstantin Savon <konstantin.savon@aterise.com>
2021-10-24 23:05:57 +0300

    Change title

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React + Kubernatis + GKE
+            Fib Calculator on GKE
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
