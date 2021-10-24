8d242370e5020340b2582d692737802a5bd3d40e
Konstantin Savon <konstantin.savon@aterise.com>
2021-10-24 22:20:50 +0300

    Add deployment script

---
+++ deploys.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+docker build -t kswest.azurecr.io/ksavon/multi-client:latest -t kswest.azurecr.io/ksavon/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t kswest.azurecr.io/ksavon/multi-server:latest -t kswest.azurecr.io/ksavon/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t kswest.azurecr.io/ksavon/multi-worker:latest -t kswest.azurecr.io/ksavon/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push kswest.azurecr.io/ksavon/multi-client:latest
+docker push kswest.azurecr.io/ksavon/multi-server:latest
+docker push kswest.azurecr.io/ksavon/multi-worker:latest
+
+docker push kswest.azurecr.io/ksavon/multi-client:$SHA
+docker push kswest.azurecr.io/ksavon/multi-server:$SHA
+docker push kswest.azurecr.io/ksavon/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployemnts/server-deployment server=kswest.azurecr.io/ksavon/multi-server:$SHA
+kubectl set image deployemnts/client-deployment client=kswest.azurecr.io/ksavon/multi-client:$SHA
+kubectl set image deployemnts/worker-deployment worker=kswest.azurecr.io/ksavon/multi-worker:$SHA
